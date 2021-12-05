c6b7bdb08e57ab420d0d9336ea5735791e6e3171
pintowb18 <pintowb18@gmail.com>
2021-12-05 20:47:16 +0000

    2ndtrb 29/11

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -4,12 +4,20 @@ fun Canvas.drawGame(game: Game) {
     erase()
     drawSpaceShip(game)
     drawAlienShots(game)
+    if (game.over == true) drawText(game.area.width / 2, SPACESHIP_BASE_LINE, "GAME OVER", RED )
 }
 
 fun Canvas.drawSpaceShip(game: Game) {
     drawRect(game.ship.x, game.ship.y, game.ship.width, game.ship.height, game.ship.color)
-    drawRect(game.ship.x + SPACESHIP_WIDTH / 2, game.ship.y - GUN_HEIGHT, GUN_WIDTH, GUN_HEIGHT, GUN_COLOR)
+    drawRect(
+        game.ship.x + SPACESHIP_WIDTH / 2 - GUN_WIDTH / 2,
+        game.ship.y - GUN_HEIGHT,
+        GUN_WIDTH,
+        GUN_HEIGHT,
+        GUN_COLOR
+    )
 }
+
 fun Canvas.drawShot(game: Game) {
     drawRect(game.shipShot.x, game.shipShot.y, GUN_SHOT_WIDHT, GUN_SHOT_HEIGHT, GUN_SHOT_COLOR)
 }
@@ -20,20 +28,53 @@ fun Canvas.drawAlienShots(game: Game) {
     }
 }
 
-fun Game.shotHit() = Game(area, alienShots.filter {it.x != shipShot.x && it.y != shipShot.y},shipShot, ship)
+fun Game.shotHit() = Game(
+    area,
+    alienShots.filter { !shipShot.getBoundingBox().intersectsWith(it.getBoundingBox()) },
+    shipShot,
+    ship
+)
+
+fun Game.gameOver() = Game(
+    area,
+    alienShots,
+    shipShot,
+    ship,
+    over = !(alienShots.all { !it.getBoundingBox().intersectsWith(ship.getBoundingBox()) })
+    )
+
+
+
+
+fun BoundingBox.intersectsWith(box: BoundingBox) =
+    corner.x in box.corner.x..box.corner.x + box.width && corner.y in box.corner.y..box.corner.y + box.height ||
+            box.corner.x in corner.x..corner.x + width && box.corner.y in corner.y..corner.y + height
+
+
+fun Spaceship.getBoundingBox() = BoundingBox(Position(x, y), width, height)
+
+fun Shot.getBoundingBox() = BoundingBox(Position(x, y), GUN_SHOT_WIDHT, GUN_SHOT_HEIGHT)
 
 
 fun Game.moveSpaceship(mouseX: Int) = copy(ship = ship.copy(x = mouseX - ship.width / 2))
 
-fun Game.moveAlienShot() = Game(area, alienShots.map { Shot(x = it.x, y = it.y + it.speed, speed = it.speed) }, shipShot, ship)
+fun Game.moveAlienShot() =
+    Game(area, alienShots.map { Shot(x = it.x, y = it.y + it.speed, speed = it.speed) }, shipShot, ship)
 
-fun Game.moveShot() = Game(area, alienShots, Shot(x = shipShot.x, y = shipShot.y - shipShot.speed, speed = shipShot.speed), ship)
+fun Game.moveShot() =
+    Game(area, alienShots, Shot(x = shipShot.x, y = shipShot.y - shipShot.speed, speed = shipShot.speed), ship)
 
-fun Game.shot(mouseX: Int) = if (shipShot.y < 0) copy(shipShot = Shot(mouseX, ship.y - SPACESHIP_HEIGHT - GUN_HEIGHT, SHIP_SHOT_SPEED)) else copy()
+fun Game.shot(mouseX: Int) = if (shipShot.y < 0) copy(
+    shipShot = Shot(
+        mouseX,
+        ship.y - SPACESHIP_HEIGHT - GUN_HEIGHT,
+        SHIP_SHOT_SPEED
+    )
+) else copy()
 
-fun Game.addAlienShot() = if (alienShots.size < 6)  copy(alienShots = (alienShots + Shot())) else copy()
+fun Game.addAlienShot() =  copy(alienShots = (alienShots + Shot()))
 
-fun Game.removeAlienShot() = Game(area, alienShots.filter { it.y <= CANVAS_HEIGHT },shipShot, ship)
+fun Game.removeAlienShot() = Game(area, alienShots.filter { it.y <= CANVAS_HEIGHT }, shipShot, ship)
 
 fun isOnLimit(mouseEvent: MouseEvent): Boolean =
     (mouseEvent.x < CANVAS_WIDTH - SPACESHIP_WIDTH / 2 && mouseEvent.x > SPACESHIP_WIDTH / 2)
--- src/main/kotlin/MainApp.kt
+++ src/main/kotlin/MainApp.kt
@@ -1,25 +1,25 @@
 import pt.isel.canvas.*
 
 fun main() {
-    onStart {
-        val canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT, BACKGROUND_COLOR)
-        var game = Game(
-            Area(canvas.width, canvas.height),
-            emptyList(),
-            Shot(),
-            Spaceship(SPACESHIP_WIDTH, SPACESHIP_HEIGHT, canvas.width / 2, SPACESHIP_BASE_LINE, SPACESHIP_COLOR)
-        )
 
+    val canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT, BACKGROUND_COLOR)
+
+    var game = Game(
+        Area(canvas.width, canvas.height),
+        emptyList(),
+        Shot(),
+        Spaceship(SPACESHIP_WIDTH, SPACESHIP_HEIGHT, canvas.width / 2, SPACESHIP_BASE_LINE, SPACESHIP_COLOR)
+    )
+    onStart {
         canvas.drawGame(game)
 
-        canvas.onTimeProgress(7){
-            game = game.addAlienShot()
-            game = game.removeAlienShot()
-            game = game.moveAlienShot()
-            game = game.moveShot()
-            game = game.shotHit()
+        canvas.onTimeProgress(FPS){
+            game = game.addAlienShot().removeAlienShot().moveAlienShot().moveShot().shotHit().gameOver()
             canvas.drawGame(game)
             canvas.drawShot(game)
+        }
+        canvas.onTimeProgress(250) {
+            game = game.addAlienShot()
         }
 
         canvas.onMouseMove {
@@ -31,4 +31,3 @@ fun main() {
     }
     onFinish { }
 }
-
